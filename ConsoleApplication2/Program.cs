8b33f7424147bc64246286db964be498ad9f278b
Lazy Rabbit <lazy_rabbit@livemail.tw>
2016-02-21 13:06:09 +0800

    20160221_02

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tt = "123";
+            ////[條件式註解語法]
+            //‪#‎if‬ DEBUG && STAGE
+            //var tt = new RuntimeTextTemplate1().TransformText();
+            //‪#‎elif‬ !DEBUG
+            //var tt = "Not Debug"
+            //‪#‎else‬
+            //var tt = "123";
+            //‪#‎endif‬
+
+            Debug.WriteLine("Start");
+
+            throw new Exception(tt);
+
+            Debug.WriteLine("END");
+
+        }
+
+        private string DoSomething(string tt)
+        { return tt; }
+    }
+}
